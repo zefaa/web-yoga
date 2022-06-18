d0e86a2a4c865344b94ce2b726f4b1c8043cc6e4
imavi3 <imavi3@icloud.com>
2022-06-18 10:14:06 +0700

    tes2

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,7 +62,7 @@ export default {
 
     // Build Configuration (https://go.nuxtjs.dev/config-build)
     build: {
-        target: 'static',
+         target: 'static',
         extend (config, ctx) {
         },
     },
